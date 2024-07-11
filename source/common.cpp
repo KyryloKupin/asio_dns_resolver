6cc286f4a33f73759674c0750af334b12ba0079f
Kyrylo Kupin <95937194+KyryloKupin@users.noreply.github.com>
2024-07-11 10:50:30 +0300

    Refactor type casting and initialization in common.cpp
    The type casting and variable initialization in common.cpp has been refactored. Specifically, in the method 'to_dns_label_format', the code for converting start and end positions to their respective difference types have been optimized. Similarly, changes have been made to the way 'len' and 'next_byte' are read and used in 'read_label_from_stream' and the subsequent loop respectively.

--- source/common.cpp
+++ source/common.cpp
@@ -6,23 +6,25 @@ using namespace tuposoft;
 
 auto tuposoft::to_dns_label_format(const std::string &domain) -> std::vector<std::uint8_t> {
     auto label_format = std::vector<std::uint8_t>{};
-    std::size_t start{};
+    auto start = std::size_t{};
 
     // ReSharper disable once CppDFAUnusedValue
-    for (std::size_t end{}; (end = domain.find('.', start)) != std::string::npos; start = end + 1) {
+    for (auto end = std::size_t{}; (end = domain.find('.', start)) != std::string::npos; start = end + 1) {
         auto length = static_cast<std::uint8_t>(end - start);
         label_format.push_back(length);
-        label_format.insert(label_format.end(), domain.begin() + static_cast<long int>(start),
-                            domain.begin() + static_cast<long long int>(end));
+        label_format.insert(label_format.end(), domain.begin() + static_cast<std::string::difference_type>(start),
+                            domain.begin() + static_cast<std::string::difference_type>(end));
     }
 
     if ((static_cast<unsigned>(domain.at(0)) & UPPER_TWO_BITS_MASK) == UPPER_TWO_BITS_MASK) {
-        label_format.insert(label_format.end(), domain.begin() + static_cast<long int>(start), domain.end());
+        label_format.insert(label_format.end(), domain.begin() + static_cast<std::string::difference_type>(start),
+                            domain.end());
     } else {
         // Last label
         const auto length = static_cast<std::uint8_t>(domain.size() - start);
         label_format.push_back(length);
-        label_format.insert(label_format.end(), domain.begin() + static_cast<long int>(start), domain.end());
+        label_format.insert(label_format.end(), domain.begin() + static_cast<std::string::difference_type>(start),
+                            domain.end());
         // Null terminator for the qname
         label_format.push_back(0);
     }
@@ -30,7 +32,7 @@ auto tuposoft::to_dns_label_format(const std::string &domain) -> std::vector<std
     return label_format;
 }
 
-auto read_label_from_stream(std::istream &input, const unsigned char len) -> std::string {
+auto read_label_from_stream(std::istream &input, const std::uint8_t len) -> std::string {
     std::vector<char> buffer(len);
     input.read(buffer.data(), len);
     return std::string{buffer.begin(), buffer.end()} + '.';
@@ -45,7 +47,7 @@ auto tuposoft::from_dns_label_format(std::istream &input) -> std::string {
     constexpr auto UNSET_PTR_POS = -1;
     auto first_ptr_pos{UNSET_PTR_POS};
 
-    for (auto next_byte = static_cast<unsigned char>(input.get()); next_byte != 0; next_byte = input.get()) {
+    for (auto next_byte = static_cast<std::uint8_t>(input.get()); next_byte != 0; next_byte = input.get()) {
         if ((next_byte & UPPER_TWO_BITS_MASK) != UPPER_TWO_BITS_MASK) {
             result += read_label_from_stream(input, next_byte);
         } else {
