d2e01fb51c8aaa6ddae37d2eef4bb86fe5a0fc96
Kyrylo Kupin <95937194+KyryloKupin@users.noreply.github.com>
2024-07-16 18:20:23 +0300

    Add IPv6 parsing test and enhance IP parsing
    The changes enhance the function parse_rdata for both IPv4 and IPv6 addresses in dns_answer. The parsing functions now directly translate binary input into string representation. Additionally, a new test case for parsing IPv6 addresses has been added.

--- test/source/dns_answer.cpp
+++ test/source/dns_answer.cpp
@@ -13,3 +13,11 @@ TEST(dns_answer, parse_rdata_a) {
     const auto actual = parse_rdata<dns_record_e::A>(input);
     ASSERT_EQ(expected, actual);
 }
+
+TEST(dns_answer, parse_rdata_aaaa) {
+    constexpr auto expected = "2606:2800:21f:cb07:6820:80da:af6b:8b2c";
+    auto span = std::span{"&\006(\000\002\037\313\ah \200\332\257k\213,"};
+    auto input = std::istringstream{{span.begin(), span.end()}, std::ios::binary};
+    const auto actual = parse_rdata<dns_record_e::AAAA>(input);
+    ASSERT_EQ(expected, actual);
+}
