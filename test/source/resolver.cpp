ec47921ea944a8026b915a4cdcca1c58c4c529c2
Kyrylo Kupin <95937194+KyryloKupin@users.noreply.github.com>
2024-07-16 10:58:58 +0300

    Add resolver test in resolver.cpp
    A new test case has been added to resolver.cpp file. This verifies successful DNS resolver connection and correct results for a specific query. In addition, an assertion for a existing test case has been updated.

--- test/source/resolver.cpp
+++ test/source/resolver.cpp
@@ -6,6 +6,21 @@
 using namespace tuposoft;
 using namespace boost::asio;
 
+TEST(resolver_test, a) {
+    auto context = io_context{};
+    co_spawn(
+            context,
+            []() -> awaitable<void> {
+                auto resolv = resolver{co_await this_coro::executor};
+                co_await resolv.connect("1.1.1.1");
+                const auto result = co_await resolv.query<dns_record_e::A>("tuposoft.com");
+                EXPECT_EQ(result.size(), 1);
+                EXPECT_EQ(result[0].rdata, "178.151.191.58");
+            },
+            detached);
+    context.run();
+}
+
 TEST(resolver_test, mx) {
     auto context = io_context{};
     co_spawn(
@@ -15,6 +30,7 @@ TEST(resolver_test, mx) {
                 co_await resolv.connect("1.1.1.1");
                 const auto result = co_await resolv.query<dns_record_e::MX>("tuposoft.com");
                 EXPECT_EQ(result.size(), 1);
+                EXPECT_EQ(result[0].rdata.mx, "mail.tuposoft.com");
             },
             detached);
     context.run();
