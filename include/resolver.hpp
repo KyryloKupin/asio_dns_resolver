2e551c12aaf751b4ac73d1177f83f8e006a0bb98
irog-c <mihajlov.igor15@gmail.com>
2024-08-15 17:22:42 +0200

    Add timeout and wait for udp response

--- include/resolver.hpp
+++ include/resolver.hpp
@@ -1,97 +1,124 @@
 #pragma once
 
+#include "boost/asio/use_awaitable.hpp"
 #include "dns_query.hpp"
 #include "dns_response.hpp"
 #include "qtype.hpp"
 
 #include "boost/asio.hpp"
+#include "boost/asio/experimental/parallel_group.hpp"
 
 #include <random>
 #include <sstream>
 
 namespace kyrylokupin::asio::dns {
-    namespace asio = boost::asio;
+namespace asio = boost::asio;
 
-    class resolver {
-    public:
-        explicit resolver(const asio::any_io_executor &executor) : socket_(executor) {}
+class resolver {
+public:
+  explicit resolver(const asio::any_io_executor &executor)
+      : socket_(executor) {}
 
-        auto connect(const std::string server) -> asio::awaitable<void> {
-            constexpr auto DNS_PORT = 53;
-            co_await socket_.async_connect({asio::ip::address::from_string(server), DNS_PORT}, asio::use_awaitable);
-        }
+  auto connect(const std::string server) -> asio::awaitable<void> {
+    constexpr auto DNS_PORT = 53;
+    co_await socket_.async_connect(
+        {asio::ip::address::from_string(server), DNS_PORT},
+        asio::use_awaitable);
+  }
 
-        template<qtype T>
-        auto query(const std::string domain) -> asio::awaitable<std::vector<dns_answer<T>>> {
-            const auto query = create_query<T>(domain);
-            auto buf = asio::streambuf{};
-            auto out = std::ostream{&buf};
-            out << query;
-            co_await socket_.async_send(buffer(buf.data(), buf.size()), asio::use_awaitable);
+  template <qtype T>
+  auto query(const std::string domain)
+      -> asio::awaitable<std::vector<dns_answer<T>>> {
+    static constexpr auto timeout_seconds = 5;
+    static constexpr auto INPUT_BUFFER_SIZE = 1024;
+    const auto query = create_query<T>(domain);
+    auto buf = asio::streambuf{};
+    auto out = std::ostream{&buf};
+    out << query;
+    co_await socket_.async_send(buffer(buf.data(), buf.size()),
+                                asio::use_awaitable);
 
-            auto input = std::array<char, 1024>{};
-            co_await socket_.async_receive(asio::buffer(input), asio::use_awaitable);
+    auto input = std::array<char, INPUT_BUFFER_SIZE>{};
+    auto timer = asio::steady_timer{co_await asio::this_coro::executor};
+    timer.expires_after(
+        std::chrono::seconds(timeout_seconds)); // Set timeout duration
 
-            auto dns_response = kyrylokupin::asio::dns::dns_response<T>{};
-            auto instream = std::istringstream{{input.begin(), input.end()}, std::ios::binary};
-            instream >> dns_response;
+    auto receive =
+        socket_.async_receive(asio::buffer(input), asio::use_awaitable);
+    auto wait = timer.async_wait(asio::use_awaitable);
 
-            co_return dns_response.answers;
-        }
+    auto [receive_result, receive_ec, wait_result, wait_ec] =
+        co_await asio::experimental::make_parallel_group(
+            [&](auto token) {
+              return socket_.async_receive(asio::buffer(input), token);
+            },
+            [&](auto token) { return timer.async_wait(token); })
+            .async_wait(boost::asio::experimental::wait_for_one(),
+                        asio::use_awaitable);
 
-    private:
-        static auto generate_id() -> std::uint16_t;
-
-        template<qtype T>
-        auto create_query(const std::string &name) {
-            return dns_query{.header =
-                                     {
-                                             .id = generate_id(),
-                                             .rd = 0x01,
-                                             .qdcount = 0x01,
-                                     },
-                             .question = {
-                                     .qname = name,
-                                     .type = T,
-                                     .cls = qclass::INET,
-                             }};
-        }
-
-        static auto reverse_qname(const std::string &name) -> std::string {
-            auto iss = std::istringstream{name};
-            auto segment = std::string{};
-            auto segments = std::vector<std::string>{};
-
-            while (std::getline(iss, segment, '.')) {
-                segments.push_back(segment);
-            }
-
-            std::reverse(segments.begin(), segments.end());
-
-            auto reversed_ip = std::string{};
-            for (const auto &seg: segments) {
-                reversed_ip += seg + '.';
-            }
-
-            return reversed_ip;
-        }
-
-        asio::ip::udp::socket socket_;
-    };
-
-    template<>
-    inline auto resolver::create_query<qtype::PTR>(const std::string &name) {
-        const auto qname = reverse_qname(name) + "in-addr.arpa";
-        return dns_query{.header =
-                                 {
-                                         .id = generate_id(),
-                                         .rd = 0x01,
-                                         .qdcount = 0x01,
-                                 },
-                         .question = {
-                                 .qname = qname,
-                                 .type = qtype::PTR,
-                                 .cls = qclass::INET,
-                         }};
+    if (receive_result[0] == 0) {
+      auto dns_response = kyrylokupin::asio::dns::dns_response<T>{};
+      auto instream =
+          std::istringstream{{input.begin(), input.end()}, std::ios::binary};
+      instream >> dns_response;
+      co_return dns_response.answers;
+    } else {
+      throw std::runtime_error("Timeout waiting for UDP response");
     }
-} // namespace KyryloKupin::asio::dns
+  }
+
+private:
+  static auto generate_id() -> std::uint16_t;
+
+  template <qtype T> auto create_query(const std::string &name) {
+    return dns_query{.header =
+                         {
+                             .id = generate_id(),
+                             .rd = 0x01,
+                             .qdcount = 0x01,
+                         },
+                     .question = {
+                         .qname = name,
+                         .type = T,
+                         .cls = qclass::INET,
+                     }};
+  }
+
+  static auto reverse_qname(const std::string &name) -> std::string {
+    auto iss = std::istringstream{name};
+    auto segment = std::string{};
+    auto segments = std::vector<std::string>{};
+
+    while (std::getline(iss, segment, '.')) {
+      segments.push_back(segment);
+    }
+
+    std::reverse(segments.begin(), segments.end());
+
+    auto reversed_ip = std::string{};
+    for (const auto &seg : segments) {
+      reversed_ip += seg + '.';
+    }
+
+    return reversed_ip;
+  }
+
+  asio::ip::udp::socket socket_;
+};
+
+template <>
+inline auto resolver::create_query<qtype::PTR>(const std::string &name) {
+  const auto qname = reverse_qname(name) + "in-addr.arpa";
+  return dns_query{.header =
+                       {
+                           .id = generate_id(),
+                           .rd = 0x01,
+                           .qdcount = 0x01,
+                       },
+                   .question = {
+                       .qname = qname,
+                       .type = qtype::PTR,
+                       .cls = qclass::INET,
+                   }};
+}
+} // namespace kyrylokupin::asio::dns
--- standalone/source/example.cpp
+++ standalone/source/example.cpp
@@ -9,61 +9,67 @@ namespace http = beast::http;
 namespace asio = boost::asio;
 namespace dns = kyrylokupin::asio::dns;
 
-auto handle_session(asio::ip::tcp::socket socket, std::shared_ptr<dns::resolver> resolver)
-        -> asio::awaitable<void> {
-    try {
-        auto buffer = beast::flat_buffer{};
-        auto request = http::request<http::string_body>{};
-        co_await http::async_read(socket, buffer, request, asio::use_awaitable);
+auto handle_session(asio::ip::tcp::socket socket,
+                    std::shared_ptr<dns::resolver> resolver)
+    -> asio::awaitable<void> {
+  try {
+    auto buffer = beast::flat_buffer{};
+    auto request = http::request<http::string_body>{};
+    co_await http::async_read(socket, buffer, request, asio::use_awaitable);
 
-        if (request.method() == http::verb::get and request.target().starts_with("/resolve?")) {
-            const auto params = request.target().substr(9);
-            if (const auto pos = params.find("&"); pos != std::string::npos) {
-                const auto domain = params.substr(0, pos);
-                const auto query_type = params.substr(pos + 1);
+    if (request.method() == http::verb::get and
+        request.target().starts_with("/resolve?")) {
+      const auto params = request.target().substr(9);
+      if (const auto pos = params.find("&"); pos != std::string::npos) {
+        const auto domain = params.substr(0, pos);
+        const auto query_type = params.substr(pos + 1);
 
-                auto result = co_await resolver->query<dns::qtype::MX>(domain);
+        auto result = co_await resolver->query<dns::qtype::MX>(domain);
 
-                auto response = http::response<http::string_body>{http::status::ok, request.version()};
-                response.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-                response.set(http::field::content_type, "text/plain");
+        auto response = http::response<http::string_body>{http::status::ok,
+                                                          request.version()};
+        response.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        response.set(http::field::content_type, "text/plain");
 
-                auto output = std::ostringstream{};
-                for (const auto &answer: result) {
-                    auto [preference, domain] = answer.rdata;
-                    output << "Preference: " << preference << ", ";
-                    output << "MX: " << domain << ";\n";
-                }
-
-                response.body() = output.str();
-                response.prepare_payload();
-                co_await http::async_write(socket, response, asio::use_awaitable);
-            }
+        auto output = std::ostringstream{};
+        for (const auto &answer : result) {
+          auto [preference, domain] = answer.rdata;
+          output << "Preference: " << preference << ", ";
+          output << "MX: " << domain << ";\n";
         }
 
-        socket.shutdown(asio::ip::tcp::socket::shutdown_send);
-    } catch (std::exception &e) {
-        std::cerr << "Error: " << e.what() << '\n';
+        response.body() = output.str();
+        response.prepare_payload();
+        co_await http::async_write(socket, response, asio::use_awaitable);
+      }
     }
+
+    socket.shutdown(asio::ip::tcp::socket::shutdown_send);
+  } catch (std::exception &e) {
+    std::cerr << "Error: " << e.what() << '\n';
+  }
 }
 
 auto listener(asio::ip::tcp::acceptor acceptor) -> asio::awaitable<void> {
-    const auto resolver = std::make_shared<dns::resolver>(acceptor.get_executor());
-    co_await resolver->connect("1.1.1.1");
+  const auto resolver =
+      std::make_shared<dns::resolver>(acceptor.get_executor());
+  co_await resolver->connect("1.1.1.1");
 
-    for (;;) {
-        auto socket = co_await acceptor.async_accept(asio::use_awaitable);
-        co_spawn(acceptor.get_executor(), handle_session(std::move(socket), resolver), asio::detached);
-    }
+  for (;;) {
+    auto socket = co_await acceptor.async_accept(asio::use_awaitable);
+    co_spawn(acceptor.get_executor(),
+             handle_session(std::move(socket), resolver), asio::detached);
+  }
 }
 
 auto main() -> int {
-    constexpr auto PORT = 8080;
+  constexpr auto PORT = 8080;
 
-    auto io_context = asio::io_context{};
-    auto acceptor = asio::ip::tcp::acceptor{io_context, {asio::ip::tcp::v4(), PORT}};
-    co_spawn(io_context, listener(std::move(acceptor)), asio::detached);
-    io_context.run();
+  auto io_context = asio::io_context{};
+  auto acceptor =
+      asio::ip::tcp::acceptor{io_context, {asio::ip::tcp::v4(), PORT}};
+  co_spawn(io_context, listener(std::move(acceptor)), asio::detached);
+  io_context.run();
 
-    return {};
+  return {};
 }
